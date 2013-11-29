b88d122eb65b9ae75f775175a5e5bcae7445d374
Paull <paull@qq.com>
2013-11-29 17:06:27 +0800

    update site favicon for apple device

--- application/views/common/layout.php
+++ application/views/common/layout.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title><?php echo SITENAME, ' - ', $template['title']; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?php echo BASEURL; ?>assets/images/favicon.ico">
-    <link rel="icon" type="image/gif" href="<?php echo BASEURL; ?>assets/images/favicon.gif">
+    <link rel="shortcut icon" href="<?php echo STATIC_URL; ?>images/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo STATIC_URL; ?>images/favicon.png">
 <?php $this->load->view('common/styles'); ?>
 </head>
 <body>
