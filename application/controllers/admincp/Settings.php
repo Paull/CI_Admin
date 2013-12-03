9909f788d576d6c997c5b3dd1784a9cfbe961de1
Paull <paull@qq.com>
2013-12-03 17:06:11 +0800

    区域地点增删改显功能第一版本，能用

--- application/controllers/admincp/Settings.php
+++ application/controllers/admincp/Settings.php
@@ -8,12 +8,50 @@ class Settings extends MY_Controller {
     }
     
     //区域数据列表
-    public function area($parentid=0)
+    public function area()
     {
-        $parentid = intval($parentid);
-
         $this->_data['template']['title'] = '区域列表';
         $this->_data['template']['breadcrumbs'][] = array('uri'=>CLASS_URI, 'title'=>$this->_data['template']['title']);
+
+        $posts = $this->input->post();
+        if ( !empty($posts) )
+        {
+            //更新旧数据
+            $update = array();
+            $updated = 0;
+            foreach($posts['area'] as $key=>$value)
+            {
+                $update[] = array('id'=>$key, 'name'=>$value);
+            }
+            if ( !empty($update) )
+            {
+                $updated = $this->m_area->update_batch($update, 'id');
+            }
+
+            //创建新数据
+            $create = array();
+            $created = 0;
+            if ( isset($posts['newarea']) )
+            {
+                foreach($posts['newarea'] as $key=>$value)
+                {
+                    foreach($value as $val)
+                    {
+                        $create[] = array('parentid'=>$key, 'name'=>$val);
+                    }
+                }
+                if ( !empty($create) )
+                {
+                    $created = $this->m_area->insert_batch($create);
+                }
+            }
+
+            //清除缓存
+            $this->m_area->clear_cache();
+
+            //显示提示
+            $this->load->view('common/message', array('message'=>"更新{$updated}条，创建{$created}条", 'uri'=>METHOD_URI));
+        }
 
         //读取数据
         $this->_data['list'] = $this->m_area->get_tree();
--- application/views/admincp/settings/area.php
+++ application/views/admincp/settings/area.php
@@ -3,15 +3,60 @@
         <h4><?php echo $template['title']; ?></h4>
     </div>
     <div class="body">
+<?php echo form_open(''); ?>
 <?php foreach($list as $key=>$value): ?>
         <div class="row-fluid">
-            <div class="span4"><input name="area[<?php echo $key; ?>]['name']" value="<?php echo $value['name']; ?>"></div>
+            <div class="span2">
+                <input type="text" name="area[<?php echo $key; ?>]" value="<?php echo $value['name']; ?>">
+            </div>
+            <div class="span2">
+                <a href="javascript:void(0);">删除</a>
+            </div>
         </div>
 <?php foreach($value['children'] as $key1=>$value1): ?>
         <div class="row-fluid">
-            <div class="offset1 span4"><input name="area[<?php echo $key1; ?>]['name']" value="<?php echo $value1['name']; ?>"></div>
+            <div class="offset1 span2">
+                <input type="text" name="area[<?php echo $key1; ?>]" value="<?php echo $value1['name']; ?>">
+            </div>
+            <div class="span2">
+                <a href="javascript:void(0);" onclick="add_location(this, <?php echo $key1; ?>, 'after', 'offset2 span2')">添加<?php echo $value1['name']; ?>下属地点</a> | <a href="javascript:void(0);">删除</a>
+            </div>
+        </div>
+<?php foreach($value1['children'] as $key2=>$value2): ?>
+        <div class="row-fluid">
+            <div class="offset2 span2">
+                <input type="text" name="area[<?php echo $key2; ?>]" value="<?php echo $value2['name']; ?>">
+            </div>
+            <div class="span2">
+                <a href="javascript:void(0);">删除</a>
+            </div>
         </div>
 <?php endforeach; ?>
 <?php endforeach; ?>
+        <div class="row-fluid">
+            <div class="offset1 span2">
+                <a href="javascript:void(0);" onclick="add_location(this, <?php echo $key; ?>, 'before', 'offset1 span2')">添加<?php echo $value['name']; ?>下属地点</a>
+            </div>
+        </div>
+<?php endforeach; ?>
+        <div class="row-fluid">
+            <div class="span2">
+                <a href="javascript:void(0);" onclick="add_location(this, <?php echo PLACE_ID; ?>, 'before', 'span2')">添加新地点</a>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">提交</button>
+<?php echo form_close(); ?>
     </div>
 </div>
+<script>
+var add_location = function(obj, parent_id, flow, class_name){
+    if(flow == 'after')
+        $(obj).parents('div.row-fluid').after('<div class="row-fluid"><div class="'+class_name+'"><input type="text" name="newarea['+parent_id+'][]"></div><div class="span2"><a href="javascript:void(0);" onclick="cancel_location(this);">撤消</a></div></div>');
+    else if(flow == 'before')
+        $(obj).parents('div.row-fluid').before('<div class="row-fluid"><div class="'+class_name+'"><input type="text" name="newarea['+parent_id+'][]"></div><div class="span2"><a href="javascript:void(0);" onclick="cancel_location(this);">撤消</a></div></div>');
+}
+var cancel_location = function(obj)
+{
+    $(obj).parents('div.row-fluid').remove();
+}
+</script>
