59c3805b1817d7b2384ba45a25d1541766de3c1e
Paull <paull@qq.com>
2013-12-11 17:30:52 +0800

    更新测试用的email配置

--- application/config/development/email.php
+++ application/config/development/email.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 $config['protocol']  = 'smtp';
-$config['smtp_host'] = '';
-$config['smtp_user'] = '';
-$config['smtp_pass'] = '';
+$config['smtp_host'] = 'smtp.exmail.qq.com';
+$config['smtp_user'] = 'service@llbbll.com';
+$config['smtp_pass'] = '123456aAa';
 $config['smtp_port'] = 465;
 $config['smtp_crypto'] = 'ssl';
 $config['newline']   = "\r\n";
