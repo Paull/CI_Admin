33eef6eb0e89f67d5699137e7a1ac3d95ab67059
Paull <paull@qq.com>
2014-01-12 00:44:17 +0800

    权限脚本增强，有提示

--- utiles/directory_permission_reset.sh
+++ utiles/directory_permission_reset.sh
@@ -1,6 +1,8 @@
 #!/bash/sh
-#should run this script in the save directory as index.php
 
-chmod 777 uploads/tmp uploads/attachment uploads/avatar application/cache application/logs
-
-echo 'okay, done!'
+if [ ! -f index.php ]; then
+    echo 'should run this script in the save directory as index.php'
+else
+    chmod 777 uploads/tmp uploads/attachment uploads/avatar application/cache application/logs
+    echo 'okay, done!'
+fi
